147122da72656e23313091daf8727f90c572f1f4
Minh VU <ngoc.vu@polytechnique.org>
2018-08-04 00:00:53 +0100

    add script to create TFRecord, and notebook to write/load and visualize TFRecord

--- README.md
+++ README.md
@@ -2,14 +2,20 @@
 This repository is a part of the Carnd Capstone Project, we want to train a model to detect traffic light and classify the state of the traffic light in real time.
 
 ## Proposed Solution
-This problem can be framed as an Object Detection problem, we can have four labels: Red, Yellow, Green, NoLight (no traffic-light). To solve this Object Detection problem, we can use the [Tensorflow Object Detection API](https://github.com/tensorflow/models/tree/master/research/object_detection). The steps are
-
-1. Gather data: extract images from simulator or ros bags
-2. Label and annotate the images and create TFRecord file (better supporting by [Tensorflow Object Detection API](https://github.com/tensorflow/models/tree/master/research/object_detection))
-3. Transfer learning using trained model (can download from [here](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md)). We fine tune the trained model's weight using our own dataset.
-4. Export our trained model for inference and evaluate it on test-data and run it in real time.
+This problem can be framed as an Object Detection problem, we can have four labels: `Red, Yellow, Green` and `NoTrafficLight`. To solve this Object Detection problem, we can use the [Tensorflow Object Detection API](https://github.com/tensorflow/models/tree/master/research/object_detection). The steps are
+1. Setup: create an environment for this project
+2. Gather data: extract images from simulator or ros bags
+3. Label and annotate the images and create TFRecord file (better supporting by [Tensorflow Object Detection API](https://github.com/tensorflow/models/tree/master/research/object_detection))
+4. Transfer learning using trained model (can download from [here](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/detection_model_zoo.md)). We fine tune the trained model's weight using our own dataset.
+5. Export our trained model for inference and evaluate it on test-data and run it in real time.
 
 In the following we describe in details each of the above steps
+
+## Setup
+We use [Tensorflow](https://www.tensorflow.org/) and [Tensorflow Object Detection API](https://github.com/tensorflow/models/tree/master/research/object_detection), so we should following the official instruction to install
+* tensorflow 1.x.x: see [here](https://www.tensorflow.org/install/)
+* object detection api: see [here](https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/installation.md)
+* additional tools: such as `yaml`
 
 ## Gather data
 When we run in simulator, the camera is populated to `/image_color`, we can extract images from this topic by using the following command (see [here](http://wiki.ros.org/image_view) for more detail)
@@ -20,11 +26,13 @@ rosrun image_view image_saver image:=/image_color
 To extract image from  a bag file, one can follow the following [tutorial](http://wiki.ros.org/rosbag/Tutorials/Exporting%20image%20and%20video%20data).
 
 ## Label and annotate the image
-To label and annotate the image, one can use the tool such as [LabelImg](https://github.com/tzutalin/labelImg).
+To label and annotate the image, one can use the tool such as [LabelImg](https://github.com/tzutalin/labelImg). Here to save time, we download the annotated data from this [repository](https://github.com/coldKnight/TrafficLight_Detection-TensorFlowAPI) or directly from [here](https://drive.google.com/file/d/0B-Eiyn-CUQtxdUZWMkFfQzdObUE/view?usp=sharing).
 
-To save time, we download the annotated data from this [repository](https://github.com/coldKnight/TrafficLight_Detection-TensorFlowAPI) or directly from [here](https://drive.google.com/file/d/0B-Eiyn-CUQtxdUZWMkFfQzdObUE/view?usp=sharing).
+The next step is to create a `TFRecord` file from annotated data, one can look at examples from [Tensorflow Object Detection API](https://github.com/tensorflow/models/tree/master/research/object_detection) (in the `dataset_tools`). Adapt from above examples, we use `create_tfrecord.py` to create `TFRecord`.
 
-The next step is to create a TFRecord file from annotated data, one can look at [Tensorflow Object Detection API](https://github.com/tensorflow/models/tree/master/research/object_detection), there are few examples in the `dataset_tools`, we
+Note that our data has the following structure
+
+
 
 
 
---
+++ create_tfrecord.py
@@ -0,0 +1,116 @@
+import yaml
+import glob
+import os
+import sys
+import tensorflow as tf
+
+from object_detection.utils import dataset_util
+from object_detection.utils import label_map_util
+
+def dict_to_tf_example(annotated_sample,
+                       img_h,
+                       img_w,
+                       data_dir,
+                       label_map_dict):
+    height = img_h # Image height
+    width = img_w # Image width
+
+    filename = annotated_sample['filename'] # Filename of the image. Empty if image is not from file
+    with tf.gfile.GFile(os.path.join(data_dir, filename), 'rb') as fid:
+        encoded_image = fid.read()
+
+    image_format = 'jpg'.encode()
+
+    xmins = [] # List of normalized left x coordinates in bounding box (1 per box)
+    xmaxs = [] # List of normalized right x coordinates in bounding box
+    # (1 per box)
+    ymins = [] # List of normalized top y coordinates in bounding box (1 per box)
+    ymaxs = [] # List of normalized bottom y coordinates in bounding box
+    # (1 per box)
+    classes_text = [] # List of string class name of bounding box (1 per box)
+    classes = [] # List of integer class id of bounding box (1 per box)
+
+    for box in annotated_sample['annotations']:
+        # if box['occluded'] is False:
+        # print("adding box")
+        xmins.append(float(box['xmin'] / width))
+        xmaxs.append(float((box['xmin'] + box['x_width']) / width))
+        ymins.append(float(box['ymin'] / height))
+        ymaxs.append(float((box['ymin' ]+ box['y_height']) / height))
+        classes_text.append(box['class'].encode())
+        classes.append(int(label_map_dict[box['class']]))
+
+    # convert to string
+    filename = filename.encode('utf-8')
+
+    # create tf.train.Example
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(filename),
+        'image/source_id': dataset_util.bytes_feature(filename),
+        'image/encoded': dataset_util.bytes_feature(encoded_image),
+        'image/format': dataset_util.bytes_feature(image_format),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_list_feature(classes),
+    }))
+    return tf_example
+
+
+def create_tf_record(annotated_samples, img_h, img_w, data_dir, label_map_dict, out_file):
+    writer = tf.python_io.TFRecordWriter(out_file)
+    nb_samples = len(annotated_samples)
+    for i, annotated_sample in enumerate(annotated_samples):
+        tf_example = dict_to_tf_example(annotated_sample,
+                                        img_h,
+                                        img_w,
+                                        data_dir,
+                                        label_map_dict)
+        writer.write(tf_example.SerializeToString())
+
+        if i % 10 == 0:
+            sys.stdout.write('\rPercent done {:.2f}%'.format((i / nb_samples) * 100))
+
+    writer.close()
+
+    print('\nDone, TFRecord is saved to {}'.format(out_file))
+
+flags = tf.app.flags
+flags.DEFINE_string('data_dir', '', 'Root directory to dataset.')
+flags.DEFINE_string('label_map_path', '', 'Path to label map proto')
+flags.DEFINE_string('img_h', '', 'Image height')
+flags.DEFINE_string('img_w', '', 'Image width')
+flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
+FLAGS = flags.FLAGS
+
+def main(_):
+    data_dir = FLAGS.data_dir
+    yaml_file = glob.glob(os.path.join(data_dir, '*.yaml'))[0]
+    with open(yaml_file, 'rb') as f:
+        annotated_samples = yaml.load(f.read())
+
+    label_map_dict = label_map_util.get_label_map_dict(FLAGS.label_map_path)
+
+    create_tf_record(annotated_samples,
+                     int(FLAGS.img_h),
+                     int(FLAGS.img_w),
+                     data_dir,
+                     label_map_dict,
+                     FLAGS.output_path)
+
+if __name__ == '__main__':
+    '''
+    An example to use it 
+    python create_tfrecord.py --data_dir=dataset-sdcnd-capstone/data/sim_training_data \
+    --label_map_path=dataset-sdcnd-capstone/label_map.pbtxt --img_h=600 --img_w=800 \
+    --output_path=sim.record
+    
+    python create_tfrecord.py --data_dir=dataset-sdcnd-capstone/data/real_training_data \
+    --label_map_path=dataset-sdcnd-capstone/label_map.pbtxt --img_h=600 --img_w=800 \
+    --output_path=real.record
+    '''
+    tf.app.run()
