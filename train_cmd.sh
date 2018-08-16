3c5b82a937fc45cd510cff816524da0738a0d85a
Minh VU <ngoc.vu@polytechnique.org>
2018-08-17 00:22:21 +0100

    wip

--- train_cmd.sh
+++ train_cmd.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+TRAIN_TYPE=$1
+set -x
 
 python object_detection/legacy/train.py \
---pipeline_config_path=config/sim/ssd_mobilenet_v1_coco_carnd_sim.config \
---train_dir=finetuned_sim/ssd_mobilenet_v1_coco
+--pipeline_config_path=config/$TRAIN_TYPE/ssd_mobilenet_v1_coco_carnd.config \
+--train_dir=finetuned_${TRAIN_TYPE}/ssd_mobilenet_v1_coco
 
 python object_detection/legacy/train.py \
---pipeline_config_path=config/sim/ssd_inception_v2_coco_carnd_sim.config \
---train_dir=finetuned_sim/ssd_inception_v2_coco
+--pipeline_config_path=config/$TRAIN_TYPE/ssd_inception_v2_coco_carnd.config \
+--train_dir=finetuned_${TRAIN_TYPE}/ssd_inception_v2_coco
 
 python object_detection/legacy/train.py \
---pipeline_config_path=config/sim/faster_rcnn_resnet101_coco_carnd_sim.config \
---train_dir=finetuned_sim/faster_rcnn_resnet101_coco
+--pipeline_config_path=config/$TRAIN_TYPE/faster_rcnn_resnet101_coco_carnd_sim.config \
+--train_dir=finetuned_${TRAIN_TYPE}/faster_rcnn_resnet101_coco
